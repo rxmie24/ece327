8060f755cff4c30fc8533e169bae872122392206
Alex Ohn <alexohn3026@gmail.com>
2016-01-31 22:57:49 -0500

    Lab 2 Heating System Code

--- lab2/heatingsys.vhd
+++ lab2/heatingsys.vhd
@@ -25,12 +25,58 @@ end heatingsys;
 
 architecture main of heatingsys is
   signal state : heat_ty;   
+  signal tmp_diff : signed(7 downto 0);
 begin
 
-  -- insert your vhdl code here
+  tmp_diff <= i_des_temp - i_cur_temp;
+  o_heatmode <= state;
 
+  statemachine : process
+  begin
+    wait until rising_edge(i_clock);
+    if i_reset = '1' then
+      state <= off;
+    else
+      case state is
+        when off =>
+          if tmp_diff >= 5 then
+            state <= high;
+          elsif tmp_diff >= 3 then
+            state <= low;
+          else
+            state <= off;
+          end if;
+
+        when low =>
+          if tmp_diff >= 7 then
+            state <= high;
+          elsif tmp_diff > 2 then
+            state <= low;
+          else
+            state <= off;
+          end if;
+
+        when high =>
+          state <= off;
+          if tmp_diff > 3 then
+            state <= high;
+          else
+            state <= low;
+          end if;
+
+        
+        when others =>
+          state <= off;
+      end case;
+    end if;
+  end process;
 end main;
 
 -- question 1
-  --insert answer here
+  -- We have designed it to have the following basic elements:
+  -- 9 AND gates
+  -- 12 OR gates
+  -- 12 Not gates
+  -- 2 1-bit flip flop
+  -- 1 subtractor
 
--- lab2/heatingsys_tb.vhd
+++ lab2/heatingsys_tb.vhd
@@ -5,12 +5,111 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.heat_pkg.all;
 
 entity heatingsys_tb is
 end entity;
 
 architecture main of heatingsys_tb is
-
+  signal cur_temp   : signed(7 downto 0);
+  signal des_temp   : signed(7 downto 0);
+  signal reset      : std_logic;
+  signal clock      : std_logic;
+  signal heatmode   : heat_ty;
 begin
+  controller : entity work.heatingsys(main)
+    port map (
+      i_cur_temp    => cur_temp,
+      i_des_temp    => des_temp,
+      i_reset       => reset,
+      i_clock       => clock,
+      o_heatmode    => heatmode
+    );
 
+  -- Clock period: 20 ns
+  clock_process : process
+  begin
+    wait for 10 ns;
+    clock <= '0';
+    wait for 10 ns;
+    clock <= '1';
+  end process;
+
+
+  process
+  begin
+    -- Wait for output state to change from undefined to off state
+    wait until rising_edge(clock);
+    cur_temp <= to_signed(0,8); des_temp <= to_signed(0,8) ; 
+	reset <= '0';
+
+    -- Off
+    wait until rising_edge(clock);
+    cur_temp <= to_signed(-1,8); des_temp <= to_signed(1,8) ; 
+	reset <= '0';
+
+    -- Off -> Low
+    wait until rising_edge(clock);
+    cur_temp <= to_signed(-1,8); des_temp <= to_signed(2,8) ; 
+	reset <= '0';
+
+    -- Low -> Low
+    wait until rising_edge(clock);
+    cur_temp <= to_signed(1,8); des_temp <= to_signed(3,8) ; 
+	reset <= '0';
+
+    -- Low -> Off
+    wait until rising_edge(clock);
+    cur_temp <= to_signed(3,8); des_temp <= to_signed(4,8) ; 
+	reset <= '0';
+
+    -- Test Off -> High
+    wait until rising_edge(clock);
+    cur_temp <= to_signed(0,8); des_temp <= to_signed(6,8) ; 
+	reset <= '0';
+
+    -- High -> High
+    wait until rising_edge(clock);
+    cur_temp <= to_signed(2,8); des_temp <= to_signed(6,8) ; 
+	reset <= '0';
+
+    -- Test High -> Off (Actually High -> Low -> Off, needs two cycles)
+    wait until rising_edge(clock);
+    cur_temp <= to_signed(7,8); des_temp <= to_signed(7,8) ; 
+	reset <= '0';
+    wait until rising_edge(clock);
+
+    -- Test Off -> Low
+    wait until rising_edge(clock);
+    cur_temp <= to_signed(2,8); des_temp <= to_signed(5,8) ; 
+	reset <= '0';
+
+    -- Test Low -> High
+    wait until rising_edge(clock);
+    cur_temp <= to_signed(0,8); des_temp <= to_signed(7,8) ; 
+	reset <= '0';
+
+    -- High Reset
+    wait until rising_edge(clock);
+    cur_temp <= to_signed(3,8); des_temp <= to_signed(7,8) ; 
+	reset <= '1';
+
+    -- Off Reset
+    wait until rising_edge(clock);
+    cur_temp <= to_signed(0,8); des_temp <= to_signed(1,8) ; 
+	reset <= '1';
+
+    -- Off -> Low
+    wait until rising_edge(clock);
+    cur_temp <= to_signed(0,8); des_temp <= to_signed(4,8) ; 
+	reset <= '0';
+
+    -- Low -> Reset
+    wait until rising_edge(clock);
+    cur_temp <= to_signed(0,8); des_temp <= to_signed(3,8) ; 
+	reset <= '1';
+
+    wait for 400 ns;
+
+  end process;
 end architecture;
